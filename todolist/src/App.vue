a7d939e5d1256b45d9ec7c76397818a1150af5fe
Nischal58 <neeschal100@gmail.com>
2022-05-17 13:49:33 +0545

    initial commit

---
+++ todolist/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+
+  
+  <NavBar/>
+<h1>hello</h1>
+      <div class="movie-card" v-for="item in movieList" :key="item.id">
+
+      <img :src="item.background_image_original">
+      <h4>{{item.title}}</h4>
+
+          </div>
+          
+ 
+
+</template> 
+<script>
+
+
+import axios from 'axios' 
+import NavBar from './components/NavBar.vue'
+export default {
+  name: 'App',
+  
+  data(){
+    return{
+      movieList:[]
+    }
+  },
+
+  async mounted(){
+  let data =axios.get('https://yts.mx/api/v2/list_movies.json')
+  .then((response=>(this.movieList=response.data.data.movies)))
+    console.log(data)
+        },
+  components: {
+    NavBar
+  },
+
+}
+</script>
+
+<style>
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+
+}
+.movie-card img{
+  width:100%;
+    height: 300px;
+    width:200px;
+    object-fit:cover;
+    object-position:center;
+  
+   
+  
+}
+.movie-card{
+  width:200px;
+  display:inline-block ;
+  margin-left: 10px;
+}
+</style>
---
+++ todolist/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <li>YTS MOVIES</li>
+    <li>Featured</li>
+    <li>Movies</li>
+    <li>Trending</li>
+
+  </div>
+
+</template> 
+<script>
+
+export default{
+    name:"NavBar"
+}
+
+
+</script>
+
+<style >
+.container{
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 50px;
+  width: 50%;
+}
+li{
+  list-style-type: none;
+
+}
+</style>
