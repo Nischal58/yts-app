68bf8a341c94ee95ad86163f5d6c4c75b2e67c43
Nischal58 <neeschal100@gmail.com>
2022-05-18 19:11:42 +0545

    yts-app
    yts-updated

--- todolist/src/components/Home.vue
+++ todolist/src/components/Home.vue
@@ -1,12 +1,5 @@
 <template>
-    <div class="movie-card" v-for="item in movieList" :key="item.id">
-
-      <img :src="item.background_image_original">
-      <h4>{{item.title}}</h4>
-
-          </div>
-          
- 
+<h1>HomePage</h1>
 </template>
 <script>
 
